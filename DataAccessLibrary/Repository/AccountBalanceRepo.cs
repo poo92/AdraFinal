16c1785f0f3f81dd65dba67ec14cbeb9e9041fad
poo92 <karunarathnepoornima@gmail.com>
2018-01-20 16:51:06 +0530

    dbcontext dependancy injection added.

---
+++ DataAccessLibrary/Repository/AccountBalanceRepo.cs
@@ -0,0 +1,78 @@
+﻿using DataAccessLibrary.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Repository
+{
+    class AccountBalanceRepo :IAccountBalanceRepo
+    {
+        private DbContext _DbContext;
+        public AccountBalanceRepo(AdraFullTestFinalContext adraFullTestFinalConext)
+        {
+            _DbContext = adraFullTestFinalConext;
+        }
+
+
+
+        // upload account balance 
+        public string UploadBalance(AccountBalance accountBalance)
+        {
+            _DbContext.AccountBalances.Add(accountBalance);
+            int result = _DbContext.SaveChanges();
+
+            if (result != 0)
+            {
+                return "uploaded succesfully";
+            }
+            else
+            {
+                return "An error occured";
+            }
+
+        }
+
+        // view balance of an account 
+        public AccountBalance ViewBalance(int year, int month)
+        {
+            AccountBalance accountBalanceresult = _DbContext.AccountBalances.Where(o => o.year == year && o.month == month).FirstOrDefault();
+            return accountBalanceresult;
+        }
+
+        // view balances of a time period
+        public List<AccountBalance> ViewBalanceChart(int startYear, int startMonth, int endYear, int endMonth)
+        {
+            List<AccountBalance> resultList = _DbContext.AccountBalances.Where(o => o.year >= startYear && o.month >= startMonth && o.year <= endYear && o.month <= endMonth).ToList<AccountBalance>();
+            return resultList;
+        }
+
+
+        public AccountBalance ViewCurrentBalance()
+        {
+            // get the last row of the accountbalance table
+            AccountBalance maximum = _DbContext.AccountBalances.OrderByDescending(o => o.year).ThenByDescending(o => o.month).FirstOrDefault();
+            return maximum;
+
+
+        }
+
+        public string DeleteAccountBalance(int year, int month)
+        {
+            AccountBalance accountToDelete = _DbContext.AccountBalances.Where(o => o.year == year && o.month == month).FirstOrDefault();
+            _DbContext.Entry(accountToDelete).State = System.Data.Entity.EntityState.Deleted;
+            int result = _DbContext.SaveChanges();
+
+            if (result != 0)
+            {
+                return "deleted successfully";
+            }
+            else
+            {
+                return "Error occured while deleting";
+            }
+        }
+    }
+}
---
+++ DataAccessLibrary/Repository/Interface/IAccountBalanceRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLibrary.Repository.Interface
+{
+    public interface IAccountBalanceRepo
+    {
+        // upload account balance
+        string UploadBalance(AccountBalance accountBalance);
+
+        // view balance of an account
+        AccountBalance ViewBalance(int year, int month);
+
+        // view balances of a time period
+        List<AccountBalance> ViewBalanceChart(int startYear, int startMonth, int endYear, int endMonth);
+
+        AccountBalance ViewCurrentBalance();
+        string DeleteAccountBalance(int year, int month);
+    }
+}
